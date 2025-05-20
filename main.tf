6014bff4dbac24b01a48f5dac0e0f5234a4852b8
Awaismalikhas <154671293+Awaismalikhas@users.noreply.github.com>
2025-05-20 16:01:46 +0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,12 @@
 # Add provider
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.0.0-beta1"
+    }
+  }
+}
 provider "aws" {
   region = "ap-south-1"
 }
